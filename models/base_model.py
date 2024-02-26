18c54363602d5682ab5fbfa6db2f040a23b0f3f2
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-27 00:39:08 +0100

    BaseModel class with detailed docstrings.

--- models/base_model.py
+++ models/base_model.py
@@ -51,7 +51,6 @@ class BaseModel:
         Updates the updated_at attribute to the current datetime and saves the instance.
         """
         self.updated_at = datetime.utcnow()
-        storage.save()
 
     def to_dict(self):
         """
