960b9be719989c835ed7a4c37d52348f2b8ec076
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-27 01:00:06 +0100

    BaseModel class with detailed docstrings.

--- models/base_model.py
+++ models/base_model.py
@@ -46,7 +46,8 @@ class BaseModel:
         Returns:
             str: String representation.
         """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        return "[{}] ({}) {}".format(
+            type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """
