6ae84791d7ba0171058eccfc6c6143fe785ec98e
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-28 12:03:24 +0100

    BaseModel class with detailed docstrings.

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,68 @@
 #!/usr/bin/python3
+"""
+BaseModel module
+
+This module defines the BaseModel class,
+which serves as the base class for other classes.
+"""
 from uuid import uuid4
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
-    """Represents the BaseModel """
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+    """
+    BaseModel class represents the base model
+    for other classes.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes a new instance of the BaseModel class.
+
+        Args:
+            *args: Variable length argument list.
+            **kwargs: Arbitrary keyword arguments.
+        """
+        if kwargs:
+            date_format = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key not in ["__class__", "created_at", "updated_at"]:
+                    setattr(self, key, value)
+                if key in {"created_at", "updated_at"}:
+                    setattr(self, key, datetime.strptime(value, date_format))
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = datetime.utcnow()
+            storage.new(self)
 
     def save(self):
-        """ updated_at take the current datetime."""
+        """
+        Updates the updated_at attribute to the
+        current datetime and saves the instance.
+        """
         self.updated_at = datetime.utcnow()
+        storage.save()
 
     def to_dict(self):
         """
-        Return the dictionary of the BaseModel instance.
-        """
-        chaima = self.__dict__.copy()
-        chaima["__class__"] = self.__class__.__name__
-        chaima["created_at"] = self.created_at.isoformat()
-        chaima["updated_at"] = self.updated_at.isoformat()
+        Converts the BaseModel instance to a dictionary.
 
-        return chaima
+        Returns:
+            dict: Dictionary representation of the instance.
+        """
+        dic = self.__dict__.copy()
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
 
     def __str__(self):
-        """Return the string representation of the BaseModel instance."""
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+        """
+        Returns a string representation of the BaseModel instance.
+
+        Returns:
+            str: String representation.
+        """
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
