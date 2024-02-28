7d421dd21d409dc9a74e2e7e9637cff1b98fe770
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-28 12:03:04 +0100

    BaseModel class with detailed docstrings.

--- models/base_model.py
+++ models/base_model.py
@@ -1,68 +1,31 @@
 #!/usr/bin/python3
-"""
-BaseModel module
-
-This module defines the BaseModel class,
-which serves as the base class for other classes.
-"""
 from uuid import uuid4
 from datetime import datetime
-from models import storage
 
 
 class BaseModel:
-    """
-    BaseModel class represents the base model
-    for other classes.
-    """
-
-    def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of the BaseModel class.
-
-        Args:
-            *args: Variable length argument list.
-            **kwargs: Arbitrary keyword arguments.
-        """
-        if kwargs:
-            date_format = "%Y-%m-%dT%H:%M:%S.%f"
-            for key, value in kwargs.items():
-                if key not in ["__class__", "created_at", "updated_at"]:
-                    setattr(self, key, value)
-                if key in {"created_at", "updated_at"}:
-                    setattr(self, key, datetime.strptime(value, date_format))
-        else:
-            self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-            storage.new(self)
+    """Represents the BaseModel """
+    def __init__(self):
+        self.id = str(uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
 
     def save(self):
-        """
-        Updates the updated_at attribute to the
-        current datetime and saves the instance.
-        """
+        """ updated_at take the current datetime."""
         self.updated_at = datetime.utcnow()
-        storage.save()
 
     def to_dict(self):
         """
-        Converts the BaseModel instance to a dictionary.
-
-        Returns:
-            dict: Dictionary representation of the instance.
+        Return the dictionary of the BaseModel instance.
         """
-        dic = self.__dict__.copy()
-        dic["__class__"] = self.__class__.__name__
-        dic["created_at"] = self.created_at.isoformat()
-        dic["updated_at"] = self.updated_at.isoformat()
-        return dic
+        chaima = self.__dict__.copy()
+        chaima["__class__"] = self.__class__.__name__
+        chaima["created_at"] = self.created_at.isoformat()
+        chaima["updated_at"] = self.updated_at.isoformat()
+
+        return chaima
 
     def __str__(self):
-        """
-        Returns a string representation of the BaseModel instance.
-
-        Returns:
-            str: String representation.
-        """
-        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+        """Return the string representation of the BaseModel instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
