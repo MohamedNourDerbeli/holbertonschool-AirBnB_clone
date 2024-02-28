18759f320b716da7349bf4d001650e700ed8d733
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-28 16:42:45 +0100

    Console module

--- console.py
+++ console.py
@@ -5,6 +5,7 @@ This module contains the HBNBCommand class which extends cmd.Cmd to create a sim
 """
 import cmd
 from models.base_model import BaseModel
+from models.user import User
 from models import storage
 
 
