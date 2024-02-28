892da6180947ddecb324c0aabc5fbbd5f32d2585
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-28 14:05:30 +0100

    Console module

--- console.py
+++ console.py
@@ -4,6 +4,8 @@
 This module contains the HBNBCommand class which extends cmd.Cmd to create a simple command-line interface.
 """
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -32,16 +34,74 @@ class HBNBCommand(cmd.Cmd):
         print("Quit command to exit the program\n")
 
     def do_quit(self, line):
+        """Usage:  quit
+        Exit the application.
         """
-        Defines the quit command to exit the program.
-        """
-        raise SystemExit
+        return True
 
-    def help_quit(self):
+    def do_create(self, line):
+        """Usage: create <classname>
+        Starts a new instance of specified class and saves it with a unique id.
+                        Prints the newly created object's id.
         """
-        Provides help information for the quit command.
+        if not line:
+            print("** class name missing **")
+            return
+        if line in globals():
+            cls = globals()[line]()
+            cls.save()
+            print(cls.id)
+        else:
+            print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """Usage: show [<classname>.<instance-id> | <classname> ]
+        Displays information about an individual object\
+or all objects of a particular class.
         """
-        print("Quit command to exit the program\n")
+        split_line = line.split()
+        if not line:
+            print("** class name missing **")
+        elif split_line[0] not in globals():
+            print("** class doesn't exist **")
+        elif len(split_line) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{split_line[0]}.{split_line[1]}"
+            obj = storage.all()
+            if key in obj:
+                print(obj[key])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, line):
+        """Destroy an object by its ID."""
+        split_line = line.split()
+        if not line:
+            print("** class name missing **")
+        elif split_line[0] not in globals():
+            print("** class doesn't exist **")
+        elif len(split_line) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{split_line[0]}.{split_line[1]}"
+            obj = storage.all()
+            if key in obj:
+                del obj[key]
+                storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, line):
+        """List all instances of a given class."""
+        class_name = line.split(".")[0]
+        if class_name not in globals().keys() and len(line) != 0:
+            print("** class doesn't exist **")
+        else:
+            obj = storage.all()
+            print([str(obj[key]) for key in obj])
+
+    # def do_update(self, line):
 
 
 if __name__ == "__main__":
