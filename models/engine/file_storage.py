d2f0988af1479a5821b2d431b5b1dc46cdd00cbb
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-28 17:52:22 +0100

    Implemented FileStorage class for serialization and deserialization of objects to/from JSON files.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -50,9 +50,13 @@ class FileStorage:
         """
         Deserializes JSON file to __objects.
         """
-        from models.user import User
         from models.base_model import BaseModel
-
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
         if isfile(self.__file_path):
             with open(self.__file_path, "r") as file:
                 load_objects = json.load(file)
