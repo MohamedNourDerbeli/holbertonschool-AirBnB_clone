68fa7400658dd0e4cedc32c63e7f2f16ef3e2c15
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-02-28 17:25:25 +0100

    Module representing the State class.

--- models/user.py
+++ models/user.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """Module representing the User class."""
 from models.base_model import BaseModel
-from models import storage
-
 
 class User(BaseModel):
     """Represents a user in the system."""
