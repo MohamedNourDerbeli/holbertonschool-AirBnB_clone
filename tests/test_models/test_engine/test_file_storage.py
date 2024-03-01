06e5e502b1ee8c24d45e4be892e8ef3356b42831
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-01 17:17:32 +0100

    test for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -20,13 +20,13 @@ class TestFileStorageClass(unittest.TestCase):
     maxDiff = None
 
     def setUp(self):
-        """ condition to test file saving """
-        with open("test.json", 'w'):
+        """condition to test file saving"""
+        with open("test.json", "w"):
             FileStorage._FileStorage__file_path = "test.json"
             FileStorage._FileStorage__objects = {}
 
     def tearDown(self):
-        """ destroys created file """
+        """destroys created file"""
         FileStorage._FileStorage__file_path = "file.json"
         try:
             os.remove("test.json")
@@ -34,35 +34,36 @@ class TestFileStorageClass(unittest.TestCase):
             pass
 
     def test_module_doc(self):
-        """ check for module documentation """
+        """check for module documentation"""
         self.assertTrue(len(file_storage.__doc__) > 0)
 
     def test_class_doc(self):
-        """ check for documentation """
+        """check for documentation"""
         self.assertTrue(len(FileStorage.__doc__) > 0)
 
     def test_method_docs(self):
-        """ check for method documentation """
+        """check for method documentation"""
         for func in dir(FileStorage):
             self.assertTrue(len(func.__doc__) > 0)
 
     def test_pep8(self):
-        """ test base and test_base for pep8 conformance """
+        """test base and test_base for pep8 conformance"""
         style = pep8.StyleGuide(quiet=True)
-        file1 = 'models/engine/file_storage.py'
-        file2 = 'tests/test_models/test_engine/test_file_storage.py'
+        file1 = "models/engine/file_storage.py"
+        file2 = "tests/test_models/test_engine/test_file_storage.py"
         result = style.check_files([file1, file2])
-        self.assertEqual(result.total_errors, 0,
-                         "Found code style errors (and warning).")
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warning)."
+        )
 
     def test_all(self):
-        """ Test method all from filestorage """
+        """Test method all from filestorage"""
         my_obj = FileStorage()
         my_dict = my_obj.all()
         self.assertTrue(type(my_dict) is dict)
 
     def test_new(self):
-        """ Tests method new for filestorage """
+        """Tests method new for filestorage"""
         my_obj = FileStorage()
         new_obj = BaseModel()
         my_obj.new(new_obj)
@@ -71,7 +72,7 @@ class TestFileStorageClass(unittest.TestCase):
         self.assertTrue(key in my_dict)
 
     def test_empty_reload(self):
-        """ Empty reload function """
+        """Empty reload function"""
         my_obj = FileStorage()
         new_obj = BaseModel()
         my_obj.new(new_obj)
@@ -83,7 +84,7 @@ class TestFileStorageClass(unittest.TestCase):
         self.assertTrue(my_dict2 == my_dict1)
 
     def test_save(self):
-        """ Tests the save method for filestorage """
+        """Tests the save method for filestorage"""
         my_obj = FileStorage()
         new_obj = BaseModel()
         my_obj.new(new_obj)
@@ -98,9 +99,29 @@ class TestFileStorageClass(unittest.TestCase):
         self.assertEqual(my_dict1[key1].to_dict(), my_dict2[key2].to_dict())
 
     def test_instance(self):
-        """ Check storage """
+        """Check storage"""
         self.assertIsInstance(storage, FileStorage)
 
+    def test_file_path(self):
+        """
+        Test to see if the file_self.path exist
+        """
+        try:
+            self.assertEqual(FileStorage._FileStorage__file_path, self.path)
+        except AttributeError:
+            pass
 
-if __name__ == '__main__':
+    def test_objects_exist_storage(self):
+        """
+        Test if __objects exist and was created
+        """
+        dic = self.storage.all()
+        try:
+            self.assertEqual(FileStorage._FileStorage__objects, dic)
+            self.assertTrue(FileStorage._FileStorage__objects)
+        except AttributeError:
+            pass
+
+
+if __name__ == "__main__":
     unittest.main()
