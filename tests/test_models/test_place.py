90a40e767e7450315c1ab99d5bcdbf436467608b
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-01 17:37:35 +0100

    test for place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,68 +1,67 @@
 #!/usr/bin/python3
-"""Unit test for the class Place
-"""
+"""Test Place"""
 import unittest
-
-# import json
-from models import place
-from models.place import Place
+import pep8
 from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.state import State
+from models.review import Review
+from models.user import User
 
 
-class TestPlaceClass(unittest.TestCase):
-    """TestPlaceClass test suit for the place class
-    Args:
-        unittest (): Propertys for unit testing
-    """
+class Testplace(unittest.TestCase):
 
-    maxDiff = None
+    def test_pep8_conformance_place(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/place.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def setUp(self):
-        """Return to "" class attributes"""
-        Place.city_id = ""
-        Place.user_id = ""
-        Place.name = ""
-        Place.description = ""
-        Place.number_rooms = 0
-        Place.number_bathrooms = 0
-        Place.max_guest = 0
-        Place.price_by_night = 0
-        Place.latitude = 0.0
-        Place.longitude = 0.0
-        Place.amenity_ids = []
+    def test_class(self):
+        place1 = Place()
+        self.assertEqual(place1.__class__.__name__, "Place")
 
-    def test_module_doc(self):
-        """check for module documentation"""
-        self.assertTrue(len(place.__doc__) > 0)
+    def test_father(self):
+        place1 = Place()
+        self.assertTrue(issubclass(place1.__class__, BaseModel))
 
-    def test_class_doc(self):
-        """check for documentation"""
-        self.assertTrue(len(Place.__doc__) > 0)
-
-    def test_method_docs(self):
-        """check for method documentation"""
-        for func in dir(Place):
-            self.assertTrue(len(func.__doc__) > 0)
-
-    def test_is_instance(self):
-        """Test if user is instance of basemodel"""
+    def test_place(self):
+        """
+        Test attributes of Class Place
+        """
+        my_amenity = Amenity()
+        my_city = City()
+        my_user = User()
         my_place = Place()
-        self.assertTrue(isinstance(my_place, BaseModel))
-
-    def test_field_types(self):
-        """Test field attributes of user"""
-        self.assertTrue(type(Place.user_id) is str)
-        self.assertTrue(type(Place.city_id) is str)
-        self.assertTrue(type(Place.name) is str)
-        self.assertTrue(type(Place.description) is str)
-        self.assertTrue(type(Place.number_rooms) is int)
-        self.assertTrue(type(Place.number_bathrooms) is int)
-        self.assertTrue(type(Place.max_guest) is int)
-        self.assertTrue(type(Place.price_by_night) is int)
-        self.assertTrue(type(Place.latitude) is float)
-        self.assertTrue(type(Place.longitude) is float)
-        self.assertTrue(type(Place.amenity_ids) is list)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        my_place.city_id = my_city.id
+        my_place.user_id = my_user.id
+        my_place.name = 'Coworking'
+        my_place.description = 'description'
+        my_place.number_rooms = 4
+        my_place.number_bathrooms = 2
+        my_place.max_guest = 4
+        my_place.price_by_night = 200
+        my_place.latitude = 25.0342808
+        my_place.longitude = -77.3962784
+        my_place.amenity_ids = str(my_amenity.id)
+        self.assertEqual(my_place.city_id, my_city.id)
+        self.assertEqual(my_place.user_id, my_user.id)
+        self.assertEqual(my_place.name, 'Coworking')
+        self.assertEqual(my_place.description, 'description')
+        self.assertEqual(my_place.number_rooms, 4)
+        self.assertTrue(type(my_place.number_rooms), int)
+        self.assertEqual(my_place.number_bathrooms, 2)
+        self.assertTrue(type(my_place.number_bathrooms), int)
+        self.assertEqual(my_place.max_guest, 4)
+        self.assertTrue(type(my_place.max_guest), int)
+        self.assertEqual(my_place.price_by_night, 200)
+        self.assertTrue(type(my_place.price_by_night), int)
+        self.assertEqual(my_place.latitude, 25.0342808)
+        self.assertTrue(type(my_place.latitude), float)
+        self.assertEqual(my_place.longitude, -77.3962784)
+        self.assertTrue(type(my_place.longitude), float)
+        self.assertEqual(my_place.amenity_ids, str(my_amenity.id))
+        self.assertTrue(type(my_place.amenity_ids), str)
