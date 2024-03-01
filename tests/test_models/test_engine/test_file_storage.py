7d9e7914034283560744d13dfbe81da5ae9c3cb8
MohamedNourDerbeli <derbelnourmohamed@gmail.com>
2024-03-01 18:30:01 +0100

    test for FileStorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -17,6 +17,7 @@ from models.user import User
 import json
 import os
 import unittest
+import pep8
 from models import storage
 
 classes = {
@@ -46,6 +47,27 @@ class TestFileStorageDocs(unittest.TestCase):
             len(FileStorage.__doc__) >= 1, "file_storage.py needs a docstring"
         )
 
+    def test_pep8_conformance_file_storage(self):
+        """Test that models/engine/file_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(["models/engine/file_storage.py"])
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings)."
+        )
+
+    def test_pep8_conformance_test_file_storage(self):
+        """Test tests/test_models/test_file_storage.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(
+            [
+                "tests/test_models/test_engine/\
+test_file_storage.py"
+            ]
+        )
+        self.assertEqual(
+            result.total_errors, 0, "Found code style errors (and warnings)."
+        )
+
     def test_file_storage_class_docstring(self):
         """Test for the FileStorage class docstring"""
         self.assertIsNot(
@@ -60,8 +82,12 @@ class TestFileStorageDocs(unittest.TestCase):
         """Test for the presence of docstrings in FileStorage methods"""
         for func in self.fs_f:
             self.assertIsNot(
-                func[1].__doc__, None, "{:s} \
-method needs a docstring".format(func[0])
+                func[1].__doc__,
+                None,
+                "{:s} \
+method needs a docstring".format(
+                    func[0]
+                ),
             )
             self.assertTrue(
                 len(func[1].__doc__) >= 1,
